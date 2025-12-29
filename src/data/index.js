ed6d2cdf87a8983ffb301daedee31248fb2920ef
yxpjseo <yjins2004@gmail.com>
2025-12-30 07:43:28 +0900

    ✨ API 및 데이터 파일 추가: axios 인스턴스 설정, 프로젝트, 멤버, 리뷰 데이터 파일 생성

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import projects from "./projects.json";
+import members from "./members.json";
+import reviews from "./reviews.json";
+
+export {
+  projects,
+  members,
+  reviews,
+};
+
+//사용하는 페이지에서 import { projects, members } from "@/data"; 이렇게 불러와서 사용
